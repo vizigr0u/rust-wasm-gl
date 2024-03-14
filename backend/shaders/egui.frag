0f67e1ac3713434f139ccdd6152cb391f140c4de
Vladimir Nachbaur <vizigr0u@gmail.com>
2024-03-13 23:51:58 -0400

    fix egui render

--- backend/shaders/egui.frag
+++ backend/shaders/egui.frag
@@ -8,9 +8,21 @@ uniform sampler2D u_texture;
 
 out vec4 outColor;
 
+vec3 srgb_gamma_from_linear(vec3 rgb) {
+    bvec3 cutoff = lessThan(rgb, vec3(0.0031308f));
+    vec3 lower = rgb * vec3(12.92f);
+    vec3 higher = vec3(1.055f) * pow(rgb, vec3(1.0f / 2.4f)) - vec3(0.055f);
+    return mix(higher, lower, vec3(cutoff));
+}
+
+vec4 srgba_gamma_from_linear(vec4 rgba) {
+    return vec4(srgb_gamma_from_linear(rgba.rgb), rgba.a);
+}
+
 void main() {
     // outColor = v_color;
     // outColor = vec4(v_texcoord.x, v_texcoord.y, 1.0f, 1.0f);
     vec4 textureColor = texture(u_texture, v_texcoord);
-    outColor = textureColor * v_color;
+    vec4 texture_in_gamma = srgba_gamma_from_linear(textureColor);
+    outColor = texture_in_gamma * v_color;
 }
--- backend/src/eguibackend.rs
+++ backend/src/eguibackend.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, rc::Rc};
 
-use egui::{epaint::Primitive, TextureId};
+use egui::{epaint::Primitive, TextureFilter, TextureId, TextureWrapMode};
 use glow::{HasContext, WebTextureKey};
 use log::{info, warn};
 
@@ -60,9 +60,21 @@ impl EguiBackend {
             .tessellate(full_output.shapes, full_output.pixels_per_point);
 
         unsafe {
-            gl.enable(glow::CULL_FACE);
-            self.paint(gl, full_output.textures_delta, clipped_primitives);
             gl.disable(glow::CULL_FACE);
+            gl.enable(glow::BLEND);
+            gl.blend_equation_separate(glow::FUNC_ADD, glow::FUNC_ADD);
+            gl.blend_func_separate(
+                // egui outputs colors with premultiplied alpha:
+                glow::ONE,
+                glow::ONE_MINUS_SRC_ALPHA,
+                // Less important, but this is technically the correct alpha blend function
+                // when you want to make use of the framebuffer alpha (for screenshots, compositing, etc).
+                glow::ONE_MINUS_DST_ALPHA,
+                glow::ONE,
+            );
+            self.paint(gl, full_output.textures_delta, clipped_primitives);
+            gl.disable(glow::BLEND);
+            gl.enable(glow::CULL_FACE);
         }
     }
 
@@ -87,7 +99,7 @@ impl EguiBackend {
                             }
                         }
                     }
-                    let mesh = self.make_mesh(&p);
+                    let mesh = Rc::new(Mesh::from(p));
                     self.mesh_renderer
                         .set_mesh(gl, mesh)
                         .expect("Can't set egui mesh");
@@ -108,32 +120,32 @@ impl EguiBackend {
     }
 
     fn make_projection(&self, width: f32, height: f32) -> glam::Mat4 {
-        glam::Mat4::orthographic_rh(0.0, width, 0.0, height, -1.0, 1.0)
+        glam::Mat4::orthographic_rh_gl(0.0, width, height, 0.0, -1.0, 1.0)
     }
 
-    fn make_mesh(&self, mesh: &egui::Mesh) -> Rc<Mesh> {
-        let mut data = Vec::<f32>::with_capacity(mesh.vertices.len() * 8);
-        for v in &mesh.vertices {
-            data.push(v.pos.x);
-            data.push(v.pos.y);
-            data.push(v.uv.x);
-            data.push(v.uv.y);
-            data.push(v.color.r() as f32 / 255.0);
-            data.push(v.color.g() as f32 / 255.0);
-            data.push(v.color.b() as f32 / 255.0);
-            data.push(v.color.a() as f32 / 255.0);
-        }
-        Rc::new(Mesh {
-            data,
-            primitive_type: glow::TRIANGLES,
-            layout: vec![
-                (VertexAttrType::Position, 2),
-                (VertexAttrType::UVs, 2),
-                (VertexAttrType::Color, 4),
-            ],
-            indices: Some(mesh.indices.clone()),
-        })
-    }
+    // fn make_mesh(&self, mesh: &egui::Mesh) -> Rc<Mesh> {
+    //     let mut data = Vec::<f32>::with_capacity(mesh.vertices.len() * 8);
+    //     for v in &mesh.vertices {
+    //         data.push(v.pos.x);
+    //         data.push(v.pos.y);
+    //         data.push(v.uv.x);
+    //         data.push(v.uv.y);
+    //         data.push(v.color.r() as f32 / 255.0);
+    //         data.push(v.color.g() as f32 / 255.0);
+    //         data.push(v.color.b() as f32 / 255.0);
+    //         data.push(v.color.a() as f32 / 255.0);
+    //     }
+    //     Rc::new(Mesh {
+    //         data,
+    //         primitive_type: glow::TRIANGLES,
+    //         layout: vec![
+    //             (VertexAttrType::Position, 2),
+    //             (VertexAttrType::UVs, 2),
+    //             (VertexAttrType::Color, 4),
+    //         ],
+    //         indices: Some(mesh.indices.clone()),
+    //     })
+    // }
 
     fn update_textures(&mut self, gl: &glow::Context, textures_delta: &egui::TexturesDelta) {
         unsafe {
@@ -159,18 +171,20 @@ impl EguiBackend {
                     glow::TEXTURE_MAG_FILTER,
                     filter_to_glow(options.magnification) as i32,
                 );
+                let (internal_format, src_format) = (glow::SRGB8_ALPHA8, glow::RGBA); // if not SRGB : (glow::RGBA8, glow::RGBA);
                 match &img_delta.image {
                     egui::ImageData::Color(image) => {
+                        let data: &[u8] = bytemuck::cast_slice(image.pixels.as_ref());
                         gl.tex_image_2d(
                             glow::TEXTURE_2D,
                             0,
-                            glow::RGBA as i32,
-                            image.width() as i32,
-                            image.height() as i32,
+                            internal_format as _,
+                            image.width() as _,
+                            image.height() as _,
                             0,
-                            glow::RGBA,
+                            src_format,
                             glow::UNSIGNED_BYTE,
-                            Some(&image.as_raw()),
+                            Some(data),
                         );
                         self.textures.insert(*id, key);
                     }
@@ -178,11 +192,11 @@ impl EguiBackend {
                         gl.tex_image_2d(
                             glow::TEXTURE_2D,
                             0,
-                            glow::RGBA as i32,
-                            image.width() as i32,
-                            image.height() as i32,
+                            internal_format as _,
+                            image.width() as _,
+                            image.height() as _,
                             0,
-                            glow::RGBA,
+                            src_format,
                             glow::UNSIGNED_BYTE,
                             Some(
                                 &image
@@ -220,7 +234,7 @@ impl EguiBackend {
         egui::RawInput {
             screen_rect: Some(egui::Rect {
                 min: egui::pos2(0.0, 0.0),
-                max: egui::pos2(800.0, 600.0),
+                max: egui::pos2(200.0, 200.0),
             }),
             ..Default::default()
         }
@@ -228,18 +242,71 @@ impl EguiBackend {
 }
 
 /* convert a TextureFilter to a glow filter */
-fn filter_to_glow(filter: egui::TextureFilter) -> u32 {
+fn filter_to_glow(filter: TextureFilter) -> u32 {
     match filter {
-        egui::TextureFilter::Linear => glow::LINEAR,
-        egui::TextureFilter::Nearest => glow::NEAREST,
+        TextureFilter::Linear => glow::LINEAR,
+        TextureFilter::Nearest => glow::NEAREST,
     }
 }
 
 /* convert a TextureWrapMode to a glow filter */
-fn wrap_to_glow(wrap: egui::TextureWrapMode) -> u32 {
+fn wrap_to_glow(wrap: TextureWrapMode) -> u32 {
     match wrap {
-        egui::TextureWrapMode::ClampToEdge => glow::CLAMP_TO_EDGE,
-        egui::TextureWrapMode::Repeat => glow::REPEAT,
-        egui::TextureWrapMode::MirroredRepeat => glow::MIRRORED_REPEAT,
+        TextureWrapMode::ClampToEdge => glow::CLAMP_TO_EDGE,
+        TextureWrapMode::Repeat => glow::REPEAT,
+        TextureWrapMode::MirroredRepeat => glow::MIRRORED_REPEAT,
+    }
+}
+
+// impl Into<Mesh> for egui::Mesh {
+//     fn into(self) -> Mesh {
+//         let mesh = self;
+//         let mut data = Vec::<f32>::with_capacity(mesh.vertices.len() * 8);
+//         for v in &mesh.vertices {
+//             data.push(v.pos.x);
+//             data.push(v.pos.y);
+//             data.push(v.uv.x);
+//             data.push(v.uv.y);
+//             data.push(v.color.r() as f32 / 255.0);
+//             data.push(v.color.g() as f32 / 255.0);
+//             data.push(v.color.b() as f32 / 255.0);
+//             data.push(v.color.a() as f32 / 255.0);
+//         }
+//         Mesh {
+//             data,
+//             primitive_type: glow::TRIANGLES,
+//             layout: vec![
+//                 (VertexAttrType::Position, 2),
+//                 (VertexAttrType::UVs, 2),
+//                 (VertexAttrType::Color, 4),
+//             ],
+//             indices: Some(mesh.indices.clone()),
+//         }
+//     }
+// }
+
+impl From<&egui::Mesh> for Mesh {
+    fn from(mesh: &egui::Mesh) -> Self {
+        let mut data = Vec::<f32>::with_capacity(mesh.vertices.len() * 8);
+        for v in &mesh.vertices {
+            data.push(v.pos.x);
+            data.push(v.pos.y);
+            data.push(v.uv.x);
+            data.push(v.uv.y);
+            data.push(v.color.r() as f32 / 255.0);
+            data.push(v.color.g() as f32 / 255.0);
+            data.push(v.color.b() as f32 / 255.0);
+            data.push(v.color.a() as f32 / 255.0);
+        }
+        Mesh {
+            data,
+            primitive_type: glow::TRIANGLES,
+            layout: vec![
+                (VertexAttrType::Position, 2),
+                (VertexAttrType::UVs, 2),
+                (VertexAttrType::Color, 4),
+            ],
+            indices: Some(mesh.indices.clone()),
+        }
     }
 }
