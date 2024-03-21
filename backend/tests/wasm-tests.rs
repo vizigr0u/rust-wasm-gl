d31f4dd6bb99c99187fa1f6657b9ab21fe5b9d36
Vladimir Nachbaur <vizigr0u@gmail.com>
2024-03-20 22:25:24 -0400

    add failing test suite

---
+++ backend/tests/wasm-tests.rs
@@ -0,0 +1,13 @@
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+use wasm_bindgen_test::*;
+
+#[wasm_bindgen_test]
+fn pass() {
+    assert_eq!(1, 1);
+}
+
+#[wasm_bindgen_test]
+fn fail() {
+    assert_eq!(1, 2);
+}
